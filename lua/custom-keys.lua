44017dee1c3321b5a532f184145735edb16550cb
Nityanand Sharma <nityanand.official@gmail.com>
2023-07-01 17:31:41 +0530

    Update custom-keys.lua

--- lua/custom-keys.lua
+++ lua/custom-keys.lua
@@ -7,34 +7,18 @@ local map = vim.api.nvim_set_keymap
 -- map the key n to run the command :NvimTreeToggle
 map('n', '<leader>n', [[:NvimTreeToggle<CR>]], {})
 
--- nvim-dap keymappings
--- Press f5 to debug
-map('n', '<F5>', [[:lua require'dap'.continue()<CR>]], {})
--- Press CTRL + b to toggle regular breakpoint
-map('n', '<C-b>', [[:lua require'dap'.toggle_breakpoint()<CR>]], {})
+-- Define the keymap for bufferline cycling
+vim.api.nvim_set_keymap('n', '<S-Right>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Left>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 
--- nvim-dap keymappings
--- Press f5 to debug
-map('n', '<F5>', [[:lua require'dap'.continue()<CR>]], {})
--- Press CTRL + b to toggle regular breakpoint
-map('n', '<C-b>', [[:lua require'dap'.toggle_breakpoint()<CR>]], {})
--- Press CTRL + c to toggle Breakpoint with Condition
-map('n', '<C-c>', [[:lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint Condition: '))<CR>]], {})
--- Press CTRL + l to toggle Logpoint
-map('n', '<C-l>', [[:lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log Point Msg: '))<CR>]], {})
--- Pressing F10 to step over
-map('n', '<F10>', [[:lua require'dap'.step_over()<CR>]], {})
--- Pressing F11 to step into
-map('n', '<F11>', [[:lua require'dap'.step_into()<CR>]], {})
--- Pressing F12 to step out
-map('n', '<F12>', [[:lua require'dap'.step_out()<CR>]], {})
--- Press F6 to open REPL
-map('n', '<F6>', [[:lua require'dap'.repl.open()<CR>]], {})
--- Press dl to run last ran configuration (if you used f5 before it will re run it etc)
-map('n', 'dl', [[:lua require'dap'.run_last()<CR>]], {})
+-- Debugger
+vim.api.nvim_set_keymap('n','<leader>dt',':DapUiToggle<CR>',{noremap=true})
 
--- Press Ctrl+d to toggle debug mode, will remove NvimTree also
-map('n', '<C-d>', [[:NvimTreeToggle<CR> :lua require'dapui'.toggle()<CR>]], {})
+vim.api.nvim_set_keymap('n','<leader>db','DapToggleBreakpoint<CR>',{noremap=true})
 
+vim.api.nvim_set_keymap('n','<leader>dc',':DapContinue<CR>',{noremap=true})
+
+vim.api.nvim_set_keymap('n','<leader>dr',":lua require('dapui).open({reset=true}) <CR>",{noremap=true})
 -- float-term
+
 map('n', '<Leader>t', ':FloatermToggle<CR>', { silent = true })
